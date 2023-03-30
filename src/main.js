bea46bd6a087278c9533104682e0240a06438151
Trung Hieu <hieu@ltv.dev>
2023-03-30 10:07:54 +0700

    Registering global BaseCard and Using it

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import { createApp } from 'vue';
 import router from './router';
 import store from './store/index';
 
+//global components
+import BaseCard from './components/UI/BaseCard.vue';
+
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-card', BaseCard);
 
 app.mount('#app');
