200ea926e34c17500894b6898336a799634c4688
Trung Hieu <hieu@ltv.dev>
2023-04-01 05:47:28 +0700

    Creating request and Updating validation logic

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,40 +1,72 @@
 <template>
-  <fom @submit.prevent="submitForm">
-    <div class="form-control">
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{ errors: !email.isValid }">
       <label for="email">Your E-mail</label>
-      <input type="email" id="email" v-model.trim="email" />
+      <input
+        type="email"
+        id="email"
+        v-model.trim="email.val"
+        @blur="clearValidation('email')"
+      />
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ errors: !message.isValid }">
       <label for="message">Message</label>
-      <textarea rows="5" id="message" v-model.trim="message" />
+      <textarea
+        rows="5"
+        id="message"
+        v-model.trim="message.val"
+        @blur="clearValidation('message')"
+      />
     </div>
-    <p v-if="!formIsValid">Please enter a valid email and not empty message!</p>
+    <p
+      v-if="!(email.isValid && message.isValid)"
+      :class="{ errors: !(email.isValid && message.isValid) }"
+    >
+      Please enter a valid email and not empty message!
+    </p>
     <div class="actions">
       <base-button type="submit">Send Message</base-button>
     </div>
-  </fom>
+  </form>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      email: '',
-      message: '',
-      formIsValid: true,
+      email: {
+        val: '',
+        isValid: true,
+      },
+      message: {
+        val: '',
+        isValid: true,
+      },
     };
   },
   methods: {
+    clearValidation(input) {
+      if (this[input].val.length > 0) {
+        this[input].isValid = true;
+      }
+    },
     submitForm() {
       this.formIsValid = true;
       if (
-        this.email === '' ||
-        !this.email.includes('@') ||
-        this.message === ''
+        this.email.val === '' ||
+        !this.email.val.includes('@') ||
+        this.message.val === ''
       ) {
-        this.formIsValid = false;
+        this.email.isValid = false;
+        this.message.isValid = false;
         return;
       }
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email.val,
+        message: this.message.val,
+        coachId: this.$route.params.id,
+      });
+      this.$router.replace('/coaches');
     },
   },
 };
