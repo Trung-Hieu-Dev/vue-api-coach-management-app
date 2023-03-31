a5c322cb9dff0bd60e10573c4450330468200189
Trung Hieu <hieu@ltv.dev>
2023-03-31 15:56:48 +0700

    Create coach

--- src/pages/coaches/CoachRegistation.vue
+++ src/pages/coaches/CoachRegistation.vue
@@ -2,7 +2,7 @@
   <base-card>
     <section>
       <h2>Register as Coach now!</h2>
-      <coach-form></coach-form>
+      <coach-form @save-data="saveData"></coach-form>
     </section>
   </base-card>
 </template>
@@ -13,6 +13,12 @@ import CoachForm from '../../components/coaches/CoachForm';
 export default {
   components: {
     CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data); //pass data to vuex by calling actions to transform data before merging to array
+      this.$router.replace('/coaches'); //navigate to /coaches
+    },
   },
 };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  registerCoach(context, data) {
+    const coachData = {
+      id: context.rootGetters.userID,
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+    context.commit('registerCoach', coachData); // pass updated data to mutations
+  },
+};
