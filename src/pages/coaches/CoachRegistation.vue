175956f3720cd7f74515d580ae8ca75d674768f0
Trung Hieu <hieu@ltv.dev>
2023-03-31 14:50:06 +0700

    Creating & Styling CoachForm Component

--- src/pages/coaches/CoachRegistation.vue
+++ src/pages/coaches/CoachRegistation.vue
@@ -1,3 +1,18 @@
 <template>
-  <h2>Coach Register Page</h2>
+  <base-card>
+    <section>
+      <h2>Register as Coach now!</h2>
+      <coach-form></coach-form>
+    </section>
+  </base-card>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
