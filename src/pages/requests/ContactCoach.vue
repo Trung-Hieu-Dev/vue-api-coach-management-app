90b37b9eedf5c0bcc6cd6b8fec80bf388acb2e2f
Trung Hieu <hieu@ltv.dev>
2023-03-31 19:19:26 +0700

    Creating & Styling CoachContact page

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,3 +1,63 @@
 <template>
-  <h2>Contact Page</h2>
+  <fom>
+    <div class="form-control">
+      <label for="email">Your E-mail</label>
+      <input type="email" id="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea rows="5" id="message" />
+    </div>
+    <div class="actions">
+      <base-button type="submit">Send Message</base-button>
+    </div>
+  </fom>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
