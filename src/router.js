d83b4573e508d5b2cc960ce0318ff830eb9875dc
Trung Hieu <hieu@ltv.dev>
2023-04-03 22:36:51 +0700

    Adding UI Login

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import CoachRegister from './pages/coaches/CoachRegistation.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -23,6 +24,7 @@ const router = createRouter({
     },
     { path: '/register', component: CoachRegister },
     { path: '/requests', component: RequestsReceived }, // show all requests list
+    { path: '/auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
