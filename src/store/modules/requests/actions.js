cab5bd978dc901bd643520e3146353dceb85739c
Trung Hieu <hieu@ltv.dev>
2023-04-03 14:30:15 +0700

    Upgrating Post HTTP user request to server

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,33 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString,
-      coachId: payload.coachId,
+      // id: new Date().toISOString,
+      // coachId: payload.coachId,
       email: payload.email,
       message: payload.message,
     };
-    context.commit('addRequest', newRequest);
+
+    //post the request to server
+    const response = await fetch(
+      `https://vue-api-coach-management-app-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POsT',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = response.json();
+
+    // console.log(responseData);
+
+    newRequest.id = responseData.name;
+    newRequest.coachId = payload.coachId;
+
+    if (!response.ok) {
+      const error = new Error(response.message || 'Fail to send request!');
+      throw error;
+    }
+
+    context.commit('addRequest', responseData);
   },
 };
