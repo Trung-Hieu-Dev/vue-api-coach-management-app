1886f0ade8efedca60666d3ab3087a075516a931
Trung Hieu <hieu@ltv.dev>
2023-04-03 10:27:12 +0700

    Upgrating send coach to backend server with firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,32 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit('registerCoach', coachData); // pass updated data to mutations
+
+    //send coach to database on backend server
+    const response = await fetch(
+      `https://vue-api-coach-management-app-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+
+    // const responseData = response.json();
+
+    if (!response.ok) {
+      //error...
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    }); // pass updated data to mutations
   },
 };
