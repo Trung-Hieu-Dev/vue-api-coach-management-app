12bee92227084190430083533e87d3e82ad62ca8
Trung Hieu <hieu@ltv.dev>
2023-04-04 07:28:41 +0700

    Adding Signup Flow

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -59,6 +59,14 @@ export default {
         return;
       }
       //send http request to get token
+      if (this.mode === 'login') {
+        //....
+      } else {
+        this.$store.dispatch('signup', {
+          email: this.email,
+          password: this.password,
+        });
+      }
     },
     switchAuthMode() {
       if (this.mode === 'login') {
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,34 @@
-export default {};
+export default {
+  login() {},
+
+  //Signup
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-oB_MMNwul2RngQdCzIQpXMzUUIBDnQo',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to Authenticated'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+};
