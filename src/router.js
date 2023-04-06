cc1fdb319d81da7abbd703495a86f70a8fd70e07
Trung Hieu <hieu@ltv.dev>
2023-04-06 16:02:41 +0700

    Adding Navigation Guards to prevent access page directly by url

--- src/router.js
+++ src/router.js
@@ -9,6 +9,9 @@ import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 
+//store
+import store from './store/index';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -22,11 +25,30 @@ const router = createRouter({
         { path: 'contact', component: ContactCoach, name: 'contact' }, // coaches/c1/contact
       ],
     },
-    { path: '/register', component: CoachRegister },
-    { path: '/requests', component: RequestsReceived }, // show all requests list
-    { path: '/auth', component: UserAuth },
+    {
+      path: '/register',
+      component: CoachRegister,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    }, // show all requests list
+    { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
     { path: '/:notFound(.*)', component: NotFound },
   ],
+});
+
+//navigation guards
+router.beforeEach(function (to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/auth');
+  } else if (to.meta.requiresAuth && store.getters.isAuthenticated) {
+    next('/coaches');
+  } else {
+    next();
+  }
 });
 
 export default router;
